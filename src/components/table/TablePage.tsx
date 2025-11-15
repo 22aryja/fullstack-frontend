e5ae8a72851ffba1966c94ccfe1ef87026713178
22aryja <dagerson19@mail.ru>
2025-11-15 13:55:19 +0500

    feat: added ag grid table, finished lab11

--- src/components/table/TablePage.tsx
+++ src/components/table/TablePage.tsx
@@ -1,5 +1,37 @@
+import { AgGridReact } from 'ag-grid-react';
+import { useState } from 'react';
+import { type ColDef } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { employees } from './dummy';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+export type Employee = {
+    id: number;
+    name: string;
+    email: string;
+    department: string;
+    salary: number;
+    startDate: string;
+};
+
 const TablePage = () => {
-    return <div></div>;
+    const [rowData] = useState<Employee[]>(employees);
+
+    const [colDefs] = useState<ColDef<Employee>[]>([
+        { field: 'id' },
+        { field: 'name' },
+        { field: 'email' },
+        { field: 'department' },
+        { field: 'salary' },
+        { field: 'startDate' },
+    ]);
+
+    return (
+        <div className='h-full'>
+            <AgGridReact rowData={rowData} columnDefs={colDefs} />
+        </div>
+    );
 };
 
 export default TablePage;
